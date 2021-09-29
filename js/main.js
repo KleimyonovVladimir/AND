751b9870021a5ae67a708702ea8094a64473e8d1
KleimyonovVladimir <kleimionovvova@gmail.com>
2021-09-29 17:52:59 +0300

    Manual

---
+++ js/main.js
@@ -0,0 +1,12 @@
+$(document).ready(() => {
+  $('#control').click(function () {
+    $('#crossing').toggleClass('expand');
+  });
+  // $("#control").click(function () {
+  //   control.text(function () {
+  //     //change text based on condition
+  //     return $content.is(":visible") ? "свернуть" : "развернуть";
+  //   });
+  // });
+
+});
