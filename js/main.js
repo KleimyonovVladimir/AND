24317c042435b662197e265319f321ec307bb429
KleimyonovVladimir <kleimionovvova@gmail.com>
2021-09-30 16:11:29 +0300

    Control is ready

--- js/main.js
+++ js/main.js
@@ -1,25 +1,12 @@
 $(document).ready(() => {
-  $('#control').click(function () {
-    $('#crossing').toggleClass('expand');
+  $("#control").click(function () {
+    $("#crossing").toggleClass("expand");
+    if ($("#crossing").hasClass("expand")) {
+      $(".control-text").html("свернуть");
+    } else {
+      $(".control-text").html("развернуть");
+    }
   });
-
-  // $("#control").click(function () {
-  //   var any = document.getElementById('control');
-  //   document.toggle(any.innerText = "развернуть");
-  // });
-
-  function toggleTxt(elem, anotherTxt) {
-    var txt = $("#control").text();
-
-    $("#control").click(function () {
-      if ($(this).text() == txt) {
-        $(this).text(anotherTxt);
-      } else {
-        $(this).text(txt);
-      }
-    });
-  };
-  toggleTxt($("#control"), "свернуть");
 
 
   const swiper = new Swiper('.swiper', {
